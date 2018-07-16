a8c31073eac822fb2fe06a59d77b772a3da3d70c
YanYi2017 <34807478+YanYi2017@users.noreply.github.com>
2018-07-16 23:53:29 +0800

    实现异步加载高德地图

--- js/app.js
+++ js/app.js
@@ -1,4 +1,7 @@
-var map = new AMap.Map('map', {
-	zoom: 13,
-	center: [120.16428, 30.272037]
-});
+var map;
+function init() {
+	map = new AMap.Map('map', {
+		zoom: 13,
+		center: [120.16428, 30.272037]
+	});
+}
